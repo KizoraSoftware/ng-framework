f1655b0357c0a6d1511d6c11d9d97d446d7888f9
Neha Sahu <neha.sahu@kizora.com>
2022-04-29 15:00:18 +0530

    install angular material and created sidenavbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-framework';
+  title = 'Angular 13 framework';
 }
